5ae4eba4ef7baa55f94b45185b56cc8b3fa1bac0
Paul Filatow <paulfltv@gmail.com>
2020-12-14 22:30:28 +0300

    file .gitignore added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int distance <= 100_000;
+        int price = 5;
+        int ticket = distance * price;
+        int bonus = ticket / 20;
+
+    }
+}
